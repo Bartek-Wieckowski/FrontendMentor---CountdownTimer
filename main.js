bdf092d9e4256b53f95601b3b44a6c6a9a28adc8
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-06-17 10:29:11 +0200

    adding JS function counter

---
+++ main.js
@@ -0,0 +1,40 @@
+const countDay = document.querySelector(".count__days");
+const countHour = document.querySelector(".count__hours");
+const countMin = document.querySelector(".count__minutes");
+const countSec = document.querySelector(".count__seconds");
+
+// Set the date we're counting down
+const countDownDate = new Date("Jul 1, 2021 15:37:25").getTime();
+const timer = setInterval(function () {
+  // Get today's date and time
+  const now = new Date().getTime();
+
+  // Distance between now and the count down date
+  const distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = String(
+    Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  ).padStart(2, "0");
+  const minutes = String(
+    Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  ).padStart(2, "0");
+  const seconds = String(Math.floor((distance % (1000 * 60)) / 1000)).padStart(
+    2,
+    "0"
+  );
+
+  // Display the result in the html element
+  countDay.textContent = `${days}`;
+  countHour.textContent = `${hours}`;
+  countMin.textContent = `${minutes}`;
+  countSec.textContent = `${seconds}`;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(timer);
+    alert(`It's time!`);
+    document.body.style.opacity = 0;
+  }
+}, 1000);
